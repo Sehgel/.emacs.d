7e53dff78e3686f0b221173e8f87aa8c06fc31d3
Sehgel <sehgel.dev@gmail.com>
2025-10-07 03:34:50 -0300

    Fix start buffer and maximize on start up

--- init.el
+++ init.el
@@ -2,18 +2,18 @@
 (tool-bar-mode -1)
 (tooltip-mode -1)
 (menu-bar-mode -1)
-
+(add-hook 'window-setup-hook 'toggle-frame-maximized t)
 
 (load-theme 'misterioso)
 (set-face-attribute 'default nil :font "Fira Code Retina")
 
-(view-lossage)
+;;; (view-lossage)
 (setq visible-bell 1)
 
 ;;; esc always quits
-(define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
-(define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
-(global-set-key [escape] 'keyboard-quit)
+;;; (define-key minibuffer-local-map [escape] 'minibuffer-keyboard-quit)
+;;; (define-key minibuffer-local-ns-map [escape] 'minibuffer-keyboard-quit)
+;;; (define-key minibuffer-local-completion-map [escape] 'minibuffer-keyboard-quit)
+;;; (define-key minibuffer-local-must-match-map [escape] 'minibuffer-keyboard-quit)
+;;; (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
+;;; (global-set-key [escape] 'keyboard-quit)
