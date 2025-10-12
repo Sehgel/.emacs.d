f0fb9ba7bd2744e4f059d07ed8de4909334e75ae
Sehgel <sehgel.dev@gmail.com>
2025-10-11 22:11:29 -0300

    Implement move line up and down and next isearch

--- init.el
+++ init.el
@@ -18,11 +18,60 @@
 ;; (define-key minibuffer-local-isearch-map [escape] 'minibuffer-keyboard-quit)
 ;; (global-set-key [escape] 'keyboard-quit)
 
-					;KEY BINDINGS
+(add-hook 'prog-mode-hook 'display-line-numbers-mode)
+
+(defun move-text-internal (arg)
+   (cond
+    ((and mark-active transient-mark-mode)
+     (if (> (point) (mark))
+            (exchange-point-and-mark))
+     (let ((column (current-column))
+              (text (delete-and-extract-region (point) (mark))))
+       (forward-line arg)
+       (move-to-column column t)
+       (set-mark (point))
+       (insert text)
+       (exchange-point-and-mark)
+       (setq deactivate-mark nil)))
+    (t
+     (beginning-of-line)
+     (when (or (> arg 0) (not (bobp)))
+       (forward-line)
+       (when (or (< arg 0) (not (eobp)))
+            (transpose-lines arg))
+       (forward-line -1)))))
+
+(defun move-text-down (arg)
+   "Move region (transient-mark-mode active) or current line
+  arg lines down."
+   (interactive "*p")
+   (move-text-internal arg))
+
+(defun move-text-up (arg)
+   "Move region (transient-mark-mode active) or current line
+  arg lines up."
+   (interactive "*p")
+   (move-text-internal (- arg)))
+
+(global-set-key (kbd "M-<up>") 'move-text-up)
+(global-set-key (kbd "M-<down>") 'move-text-down)
+
+;KEY BINDINGS
+
+(global-unset-key (kbd "C-s"))
+(global-unset-key (kbd "C-S-s"))
+
+(global-unset-key (kbd "C-f"))
+(global-unset-key (kbd "C-S-f"))
 
 (global-set-key (kbd "C-c") 'kill-ring-save)
 (global-set-key (kbd "C-x") 'kill-region)
 (global-set-key (kbd "C-v") 'yank)
+(global-set-key (kbd "C-z") 'undo)
+(global-set-key (kbd "C-y") 'undo-redo)
 
 
-
+(global-set-key (kbd "C-f") 'isearch-forward)
+(define-key isearch-mode-map (kbd "C-f") 'isearch-repeat-forward)
+(global-set-key (kbd "C-S-f") 'isearch-backward)
+(define-key isearch-mode-map (kbd "C-S-f") 'isearch-repeat-backward)
